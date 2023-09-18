53320587bfec6c170709aa5f2fc62c3b4f9b851b
jinvicky <jinvicky@intube.kr>
2023-09-18 16:20:04 +0900

    Feat:  선택 정렬 구현

--- problem-2/problem-2.test.js
+++ problem-2/problem-2.test.js
@@ -1,4 +1,26 @@
+const exchange = (list, a,b) => {
+  [list[b], list[a]] = [list[a], list[b]];
+};
+
+const less = (a, b) => a < b;
+
+const findMinIndex = (list, startIndex) => {
+  let min = startIndex;
+
+  for (let j = startIndex + 1; j < list.length; j++) {
+    if (less(list[j], list[min])) {
+      min = j;
+    }
+  }
+  return min;
+}
+
 const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    const minIndex = findMinIndex(array, i);
+
+    exchange(array, i, minIndex);
+  }
 };
 
 test.each([
