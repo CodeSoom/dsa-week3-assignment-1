576edd62e8d6f478d55acfe8f7b8f2c88ca35735
jinvicky <jinvicky@intube.kr>
2023-09-18 17:32:55 +0900

    Feat:  삽입 정렬 구현

--- problem-3/problem-3.test.js
+++ problem-3/problem-3.test.js
@@ -1,5 +1,23 @@
-const insertionSort = (array) => {
+const exchange = (list, a,b) => {
+  [list[b], list[a]] = [list[a], list[b]];
 };
+
+const less = (a,b) => a < b;
+
+const insertionSort = (array) => {
+  const {length} = array;
+
+  for (let i = 1; i < length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (less(array[j], array[j - 1])) {
+        exchange(array, j, j - 1);
+      } else {
+        break;
+      }
+    }
+  }
+};
+
 
 test.each([
   [[5, 4, 3, 2, 1]],
