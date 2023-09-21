80e65c086664109fe23ad4efb475796d39a029f5
seungwoo Kim <kyh196201@gmail.com>
2023-09-22 00:04:05 +0900

    feat: problem-1 이미 정렬되어있을 경우 첫 패스에서 함수 종료

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,4 +1,6 @@
 const bubbleSort = (array) => {
+  let exchangeCount = 0;
+
   // i = 정렬이 완료될 원소의 개수
   // 첫 패스 때 1개의 정렬이 완료됨
   // 마지막 패스 때 array.length - 1개의 원소가 정렬됨
@@ -6,8 +8,14 @@ const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1 - i; j++) {
       if (array[j] > array[j + 1]) {
+        exchangeCount += 1;
+
         [array[j + 1], array[j]] = [array[j], array[j + 1]];
       }
+    }
+
+    if (i === 0 && exchangeCount === 0) {
+      return;
     }
   }
 };
