f74ceb6b8ffc362d9cb0f2a8b793e6904debc113
JiHo Kim <kr36510@naver.com>
2023-12-24 17:02:36 +0900

    퀵 정렬

--- problem-6/problem-6.test.js
+++ problem-6/problem-6.test.js
@@ -1,4 +1,45 @@
-const quickSort = (array) => {
+const partition = (array, start, end) => {
+  let left = start + 1;
+  let right = end;
+
+  const pivot = array[start];
+
+  while (true) {
+    while (array[left] < pivot) {
+      if (left === end) {
+        break;
+      }
+
+      left++;
+    }
+
+    while (pivot < array[right]) {
+      if (right === start) {
+        break;
+      }
+
+      right--;
+    }
+
+    if (left >= right) {
+      break;
+    }
+
+    [array[left], array[right]] = [array[right], array[left]];
+  }
+
+  [array[start], array[right]] = [array[right], array[start]];
+  return right;
+};
+
+const quickSort = (array, start = 0, end = array.length - 1) => {
+  if (start >= end) {
+    return;
+  }
+
+  const j = partition(array, start, end);
+  quickSort(array, start, j - 1);
+  quickSort(array, j + 1, end);
 };
 
 test.each([
