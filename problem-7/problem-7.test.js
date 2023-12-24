a3c958af52d70f5af93042368b3d83453e2ea57f
JiHo Kim <kr36510@naver.com>
2023-12-24 17:08:32 +0900

    퀵 정렬

--- problem-7/problem-7.test.js
+++ problem-7/problem-7.test.js
@@ -1,4 +1,32 @@
+const sink = (array, i, len) => {
+  while (2 * i <= len) {
+    let j = 2 * i;
+
+    if (j < len && array[j] < array[j + 1]) {
+      j++;
+    }
+
+    if (!(array[i] < array[j])) {
+      break;
+    }
+
+    [array[i], array[j]] = [array[j], array[i]];
+    i = j;
+  }
+};
+
 const heapSort = (array) => {
+  let len = array.length - 1;
+
+  for (let i = Math.floor(len / 2); i >= 1; i--) {
+    sink(array, i, len);
+  }
+
+  while (len > 1) {
+    [array[1], array[len]] = [array[len], array[1]];
+    len--;
+    sink(array, 1, len);
+  }
 };
 
 test.each([
