cbd7a5450220d093ee4d49b1281a58f2662cbe37
JiHo Kim <kr36510@naver.com>
2023-12-24 14:24:48 +0900

    선택 정렬

--- problem-2/problem-2.test.js
+++ problem-2/problem-2.test.js
@@ -1,4 +1,17 @@
 const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+
+    [array[i], array[min]] = [array[min], array[i]];
+  }
+
+  return array;
 };
 
 test.each([
