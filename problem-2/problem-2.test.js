6f8fe12674786c89fc6bf463fbaf8840806b4039
bereal1995 <gygud98@gmail.com>
2023-12-25 21:03:50 +0900

    feat: 선택 정렬 구현하기

--- problem-2/problem-2.test.js
+++ problem-2/problem-2.test.js
@@ -1,12 +1,36 @@
-const selectionSort = (array) => {
+const swap = (array, i, j) => {
+  [array[i], array[j]] = [array[j], array[i]];
 };
 
-test.each([
-  [[5, 4, 3, 2, 1]],
-  [[1, 2, 3, 4, 5]],
-  [[4, 2, 1, 5, 3]],
-])('주어진 배열을 오름차순으로 정렬한다', (array) => {
-  selectionSort(array);
+const findMinIndex = (array, startIndex) => {
+  let minIndex = startIndex;
 
-  expect(array).toEqual([1, 2, 3, 4, 5]);
-});
+  for (let i = startIndex; i < array.length; i++) {
+    if (array[minIndex] > array[i]) {
+      minIndex = i;
+    }
+  }
+
+  return minIndex;
+};
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    const minIndex = findMinIndex(array, i);
+
+    if (minIndex === i) {
+      continue;
+    }
+
+    swap(array, i, minIndex);
+  }
+};
+
+test.each([[[5, 4, 3, 2, 1]], [[1, 2, 3, 4, 5]], [[4, 2, 1, 5, 3]]])(
+  '주어진 배열을 오름차순으로 정렬한다',
+  (array) => {
+    selectionSort(array);
+
+    expect(array).toEqual([1, 2, 3, 4, 5]);
+  }
+);
