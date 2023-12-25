f98660a7c15fad67c97def961cc1b58910dfcf08
bereal1995 <gygud98@gmail.com>
2023-12-25 22:55:10 +0900

    feat: 삽입 정렬 구현하기

--- problem-3/problem-3.test.js
+++ problem-3/problem-3.test.js
@@ -1,12 +1,24 @@
-const insertionSort = (array) => {
+const swap = (array, i, j) => {
+  [array[i], array[j]] = [array[j], array[i]];
 };
 
-test.each([
-  [[5, 4, 3, 2, 1]],
-  [[1, 2, 3, 4, 5]],
-  [[4, 2, 1, 5, 3]],
-])('주어진 배열을 오름차순으로 정렬한다', (array) => {
-  insertionSort(array);
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (array[j] < array[j - 1]) {
+        swap(array, j, j - 1);
+      } else {
+        break;
+      }
+    }
+  }
+};
 
-  expect(array).toEqual([1, 2, 3, 4, 5]);
-});
+test.each([[[5, 4, 3, 2, 1]], [[1, 2, 3, 4, 5]], [[4, 2, 1, 5, 3]]])(
+  '주어진 배열을 오름차순으로 정렬한다',
+  (array) => {
+    insertionSort(array);
+
+    expect(array).toEqual([1, 2, 3, 4, 5]);
+  }
+);
