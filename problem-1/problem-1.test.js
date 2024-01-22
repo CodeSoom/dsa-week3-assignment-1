8a2eeed03ebbea98513c25c08325a4b4a31e4a33
whoodongpyo <vvhooping@gmail.com>
2024-01-22 18:02:20 +0900

    solve: bubble sort

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,4 +1,26 @@
+const exchange = (array, index1, index2) => {
+  [array[index1], array[index2]] = [array[index2], array[index1]];
+};
+
+const isFirstBiggerThanSecond = (a, b) => a > b;
 const bubbleSort = (array) => {
+  const { length } = array;
+
+  for (let pass = 1; pass <= length; pass++) {
+    let exchangeCount = 0;
+
+    for (let index = 0; index < length - pass; index++) {
+      if (isFirstBiggerThanSecond(array[index], array[index + 1])) {
+        exchange(array, index, index + 1);
+        exchangeCount++;
+      }
+    }
+
+    // todo : 정렬이 완료됐다면 loop 를 종료한다.
+    if (exchangeCount === 0) {
+      break;
+    }
+  }
 };
 
 test.each([
