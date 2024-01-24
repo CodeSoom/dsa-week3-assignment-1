ced3cae4a5de5219b5972b0c8a572058ea1ed072
whoodongpyo <vvhooping@gmail.com>
2024-01-24 15:54:23 +0900

    solve: insertion sort

--- problem-3/problem-3.test.js
+++ problem-3/problem-3.test.js
@@ -1,4 +1,19 @@
+const exchange = (array, index1, index2) => {
+  [array[index1], array[index2]] = [array[index2], array[index1]];
+};
+
+const isFirstLesserThanSecond = (a, b) => a < b;
 const insertionSort = (array) => {
+  for (let currentIndex = 1; currentIndex < array.length; currentIndex++) {
+    for (let comparisonIndex = currentIndex; comparisonIndex > 0; comparisonIndex--) {
+      if (isFirstLesserThanSecond(array[comparisonIndex], array[comparisonIndex - 1])) {
+        exchange(array, comparisonIndex, comparisonIndex - 1);
+      } else {
+        // todo: 비교했을 때 왼쪽보다 작지 않다면 비교를 중지하고, 다음 index 로 넘어간다.
+        break;
+      }
+    }
+  }
 };
 
 test.each([
