77419a4e48eec55d53233b7837a30db7c900e449
whoodongpyo <vvhooping@gmail.com>
2024-01-26 15:31:08 +0900

    solve: shell sort

--- problem-4/problem-4.test.js
+++ problem-4/problem-4.test.js
@@ -1,4 +1,27 @@
+const exchange = (array, index1, index2) => {
+  [array[index1], array[index2]] = [array[index2], array[index1]];
+};
+
+const isFirstLesserThanSecond = (a, b) => a < b;
+
 const shellSort = (array) => {
+  const { length } = array;
+
+  let gap = Math.floor(length / 2);
+
+  while (gap > 0) {
+    for (let index = gap; index < length; index++) {
+      for (let previousIndex = index; previousIndex >= 0; previousIndex = previousIndex - gap) {
+        if (isFirstLesserThanSecond(array[previousIndex], array[previousIndex - gap])) {
+          exchange(array, previousIndex, previousIndex - gap);
+        } else {
+          break;
+        }
+      }
+    }
+
+    gap = Math.floor(gap / 2);
+  }
 };
 
 test.each([
