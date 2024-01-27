21a006a631cacea2930d17887a6ddecb69596400
whoodongpyo <vvhooping@gmail.com>
2024-01-27 18:43:59 +0900

    solve: merge sort

--- problem-5/problem-5.test.js
+++ problem-5/problem-5.test.js
@@ -1,4 +1,39 @@
-const mergeSort = (array) => {
+const isFirstLesserThanSecond = (a, b) => a < b;
+const merge = (list, start, mid, end) => {
+  let left = start;
+  let right = mid + 1;
+
+  const temp = [...list];
+
+  for (let index = start; index <= end; index++) {
+    if (left > mid) { // 왼쪽의 정렬이 모두 끝났는가? -> 오른쪽 항목들을 채운다.
+      list[index] = temp[right];
+      right++;
+    } else if (right > end) { // 오른쪽의 정렬이 모두 끝났는가? -> 왼쪽 항목들을 채운다.
+      list[index] = temp[left];
+      left++;
+    } else if (isFirstLesserThanSecond(temp[left], temp[right])) {
+      // 왼쪽과 오른쪽의 항목 중 왼쪽 값이 더 작으면 넣는다.
+      list[index] = temp[left];
+      left++;
+    } else {
+      // 왼쪽과 오른쪽의 항목 중 오른쪽 값이 더 작으면 넣는다.
+      list[index] = temp[right];
+      right++;
+    }
+  }
+};
+
+const mergeSort = (list, start = 0, end = list.length - 1) => {
+  if (start >= end) {
+    return;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  mergeSort(list, start, mid); // 절반으로 나눈 왼쪽 리스트를 정렬
+  mergeSort(list, mid + 1, end); // 절반으로 나눈 오른쪽 리스트를 정렬
+  merge(list, start, mid, end);
 };
 
 test.each([
