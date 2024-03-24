04a76330a09750dcc7b2b4b1a6712e57e4c69f91
kimgagyeong <rkrud951117@naver.com>
2024-03-24 22:58:10 +0900

    problem-2

--- problem-2/problem-2.test.js
+++ problem-2/problem-2.test.js
@@ -1,12 +1,24 @@
 const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
 };
 
-test.each([
-  [[5, 4, 3, 2, 1]],
-  [[1, 2, 3, 4, 5]],
-  [[4, 2, 1, 5, 3]],
-])('주어진 배열을 오름차순으로 정렬한다', (array) => {
-  selectionSort(array);
+test.each([[[5, 4, 3, 2, 1]], [[1, 2, 3, 4, 5]], [[4, 2, 1, 5, 3]]])(
+  "주어진 배열을 오름차순으로 정렬한다",
+  (array) => {
+    selectionSort(array);
 
-  expect(array).toEqual([1, 2, 3, 4, 5]);
-});
+    expect(array).toEqual([1, 2, 3, 4, 5]);
+  },
+);
